6688ff4aec6b10fd125f822d5f3e3d7784d08e65
arfizurrahman <arfizrahman0@gmail.com>
2018-12-14 01:20:33 +0600

    Initial Commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetAllCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var customers = GetAllCustomers();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var customer = (Customer) customers.Find(x => x.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+             
+            return View(customer);
+        }
+        private List<Customer> GetAllCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name = "John Smith"},
+                new Customer {Id = 2, Name = "Mary Williams"}
+            };
+            return customers;
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Index()
+        {
+            var movies = GetAllMovies();
+            return View(movies);
+        }
+        public ActionResult Details(int? id)
+        {
+            var movies = GetAllMovies();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var movie = (Movie)movies.Find(x => x.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+        public ActionResult Random()
+        {
+            var movie = new Movie(){ Name = "Shreik!" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name = "Customer 1"},
+                new Customer{Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel();
+            viewModel.Movie = movie;
+            viewModel.Customers = customers;
+
+            return View(viewModel);
+        }
+
+        private List<Movie> GetAllMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie(){Id = 1, Name = "Avengers-Infinity War"},
+                new Movie(){Id = 2, Name = "Superman-Man of steel"}
+            };
+
+            return movies;
+        }
+
+        //[Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id = " + id);
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        //}
+    }
+}
