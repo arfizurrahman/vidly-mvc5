1da7b28af8e77e524c1e3475aedd84ddd348c128
arfizurrahman <arfizrahman0@gmail.com>
2018-12-18 09:31:33 +0600

    Added DTO MembershipTypeDto and modified MappingProfile

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -18,5 +18,6 @@ namespace Vidly.Dtos
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public byte MembershipTypeId { get; set; }
+        public MembershipTypeDto MembershipType { get; set; }
     }
 }
