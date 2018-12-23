a60f555b74bcd17e1e8ad3b5abeca10b8c91b07f
arfizurrahman <arfizrahman0@gmail.com>
2018-12-23 21:49:52 +0600

    Modified new rental action

--- Vidly/Controllers/Api/MovieRentalsController.cs
+++ Vidly/Controllers/Api/MovieRentalsController.cs
@@ -5,15 +5,38 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class MovieRentalsController : ApiController
     {
+        ApplicationDbContext _context = new ApplicationDbContext();
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(MovieRentalDto movieRentalDto)
         {
+            var customer = _context.Customers.Single(
+                c => c.Id == movieRentalDto.CustomerId);
+
+            var movies = _context.Movies.Where(
+                m => movieRentalDto.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
             return Ok();
         }
     }
+
 }
