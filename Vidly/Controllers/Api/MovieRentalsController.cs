cbe5aebd7b234af2a81fb03a89df5a0ea5deb8a7
arfizurrahman <arfizrahman0@gmail.com>
2018-12-23 22:36:04 +0600

    Added new view for rental

--- Vidly/Controllers/Api/MovieRentalsController.cs
+++ Vidly/Controllers/Api/MovieRentalsController.cs
@@ -20,23 +20,61 @@ namespace Vidly.Controllers.Api
                 c => c.Id == movieRentalDto.CustomerId);
 
             var movies = _context.Movies.Where(
-                m => movieRentalDto.MovieIds.Contains(m.Id));
+                m => movieRentalDto.MovieIds.Contains(m.Id)).ToList();
 
             foreach (var movie in movies)
             {
+                if (movieRentalDto.MovieIds.Count == 0)
+                return BadRequest("No Movie Ids have been given.");
+                movie.NumberAvailable--;
                 var rental = new Rental
                 {
                     Customer = customer,
                     Movie = movie,
                     DateRented = DateTime.Now
                 };
-
+                
                 _context.Rentals.Add(rental);
             }
 
             _context.SaveChanges();
             return Ok();
         }
+
+        //Defensive approach to validate edge cases
+        //public IHttpActionResult CreateNewRentals(MovieRentalDto movieRentalDto)
+        //{
+        //    if (movieRentalDto.MovieIds.Count == 0)
+        //        return BadRequest("No Movie Ids have been given.");
+
+        //    var customer = _context.Customers.Single(
+        //        c => c.Id == movieRentalDto.CustomerId);
+        //    if (customer == null)
+        //        return BadRequest("Customer Id is not valid");
+
+        //    var movies = _context.Movies.Where(
+        //        m => movieRentalDto.MovieIds.Contains(m.Id)).ToList();
+
+        //    if (movies.Count != movieRentalDto.MovieIds.Count)
+        //        return BadRequest("One or more movie ids are invalid");
+
+        //    foreach (var movie in movies)
+        //    {
+
+        //        movie.NumberAvailable--;
+        //        var rental = new Rental
+        //        {
+        //            Customer = customer,
+        //            Movie = movie,
+        //            DateRented = DateTime.Now
+        //        };
+
+        //        _context.Rentals.Add(rental);
+        //    }
+
+        //    _context.SaveChanges();
+        //    return Ok();
+        //}
     }
 
 }
